f4f2ccf035d00bf7f170000d29d4495edfd452f8
Marilsa Barros <marilsa_barros@hotmail.com>
2026-02-04 18:29:20 -0300

    Um Fluxo, Dois Resultados

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -7,7 +7,6 @@ import { generateOrderCode } from '../support/helpers'
   test.describe('Consultar Pedido', () => {
   
  
-
   test.beforeEach(async ({ page }) => {  
     // Arrange
     await page.goto('http://localhost:5173/')
@@ -21,11 +20,87 @@ import { generateOrderCode } from '../support/helpers'
   test('deve consultar um pedido aprovado', async ({ page }) => {
 
     // Test Fata
-    const order = 'VLO-3ZKFYN'
+   // const order = 'VLO-3ZKFYN'
+
+   const order = {
+    number: 'VLO-3ZKFYN',
+    status: 'APROVADO',
+    color: 'Lunar White',   
+    wheels: 'aero Wheels',
+    customer: {
+      name: 'Marilsa Barros',
+      email: 'marilsa@velo.dev',      
+    },
+    payment: 'À Vista',      
+   }
   
     
     // Act
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-3ZKFYN')
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+    
+    // Assert 
+    // validação dos elementos de forma estática item por item
+    // const toContainerPedido = page.getByRole('paragraph')
+    //   .filter({ hasText: /^Pedido/ })
+    //   .locator('..')  // Sobe para o elemento pai (a div que agrupa ambos)
+  
+    // await expect(toContainerPedido).toContainText(order, { timeout: 10_000 })
+  
+    // await expect(page.getByText('APROVADO')).toBeVisible();
+  
+    // validação dos elementos de forma dinamica com snapshot
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+  
+  })
+
+  test('deve consultar um pedido reprovado', async ({ page }) => {
+
+    // Test Fata
+    //const order = 'VLO-HXRRWE'
+
+    const order = {
+      number: 'VLO-HXRRWE',
+      status: 'REPROVADO',
+      color: 'Midnight Black',   
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'WASHINGTON SANTOS',
+        email: 'wa@gamil.com',      
+      },
+      payment: 'À Vista',      
+     }
+  
+    
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
     await page.getByRole('button', { name: 'Buscar Pedido' }).click()
     
     // Assert 
@@ -40,34 +115,34 @@ import { generateOrderCode } from '../support/helpers'
   
 
     // validação dos elementos de forma dinamica com snapshot
-    await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
       - img
       - paragraph: Pedido
-      - paragraph: ${order}
+      - paragraph: ${order.number}
       - img
-      - text: APROVADO
+      - text: ${order.status}
       - img "Velô Sprint"
       - paragraph: Modelo
       - paragraph: Velô Sprint
       - paragraph: Cor
-      - paragraph: Lunar White
+      - paragraph: ${order.color}
       - paragraph: Interior
       - paragraph: cream
       - paragraph: Rodas
-      - paragraph: aero Wheels
+      - paragraph: ${order.wheels}
       - heading "Dados do Cliente" [level=4]
       - paragraph: Nome
-      - paragraph: Marilsa Barros
+      - paragraph: ${order.customer.name}
       - paragraph: Email
-      - paragraph: marilsa@velo.dev
+      - paragraph: ${order.customer.email}
       - paragraph: Loja de Retirada
       - paragraph
       - paragraph: Data do Pedido
       - paragraph: /\\d+\\/\\d+\\/\\d+/
       - heading "Pagamento" [level=4]
-      - paragraph: À Vista
+      - paragraph: ${order.payment}
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
-      `)
+      `);  
   
   })
   
