fec7695a0d23d2035c0bc77c08fbb8f794570302
Marilsa Barros <marilsa_barros@hotmail.com>
2026-02-08 17:51:32 -0300

    IA no Processo de Teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -146,6 +146,71 @@ import { generateOrderCode } from '../support/helpers'
   
   })
   
+  test('deve consultar um pedido em analise', async ({ page }) => {
+
+    // Test Fata
+    //const order = 'VLO-HXRRWE'
+
+    const order = {
+      number: 'VLO-9VNQ7P',
+      status: 'EM_ANALISE',
+      color: 'Lunar White',   
+      wheels: 'aero Wheels',
+      customer: {
+        name: 'Pedro da Silva',
+        email: 'pedro@velo.com',      
+      },
+      payment: 'À Vista',      
+     }
+  
+    
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+    
+    // Assert 
+    // validação dos elementos de forma estática item por item
+    // const toContainerPedido = page.getByRole('paragraph')
+    //   .filter({ hasText: /^Pedido/ })
+    //   .locator('..')  // Sobe para o elemento pai (a div que agrupa ambos)
+  
+    // await expect(toContainerPedido).toContainText(order, { timeout: 10_000 })
+  
+    // await expect(page.getByText('APROVADO')).toBeVisible();
+  
+
+    // validação dos elementos de forma dinamica com snapshot
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);  
+  
+  })
+
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
     const order = generateOrderCode()
   
